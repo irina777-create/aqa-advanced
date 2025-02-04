a7ff38b45cce5e242bf8d5953429de56485f74ac
irina777-create <grigoreva108@gmail.com>
2025-02-04 19:59:26 +0200

    task1

---
+++ jsBasics/task1.js
@@ -0,0 +1,16 @@
+
+let myString = "Привіт, світ!";
+
+let myNumber = 42;
+
+let myBoolean = true;
+
+let myNull = null;
+
+let myUndefined;
+
+console.log("Рядок:", myString);
+console.log("Число:", myNumber);
+console.log("Буль:", myBoolean);
+console.log("Null:", myNull);
+console.log("Undefined:", myUndefined);
