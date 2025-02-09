0411cea17caae6cb446193d315a041b90b2e3c5a
irina777-create <grigoreva108@gmail.com>
2025-02-09 16:08:37 +0200

    task3

---
+++ lesson-5/tack3.js
@@ -0,0 +1,12 @@
+const number=5;
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number} × ${i} = ${number * i}`);
+}
+
+const number=5;
+let count = 1;
+while (count <= 10) {
+    console.log(`${number} × ${count} = ${number * count}`);
+    count++;
+}
+
