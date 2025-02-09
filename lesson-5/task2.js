7622583f844ab93ccfdb8f5ddade99d325ce1607
irina777-create <grigoreva108@gmail.com>
2025-02-09 15:37:03 +0200

    task2

---
+++ lesson-5/task2.js
@@ -0,0 +1,20 @@
+let averageGrade=85;
+let gradeGroup = Math.floor(averageGrade / 10);
+
+switch (gradeGroup) {
+    case 10:
+    case 9:
+        console.log("Відмінно");
+        break;
+    case 8:
+        console.log("Дуже добре");
+        break;
+    case 7:
+        console.log("Добре");
+        break;
+    case 6:
+        console.log("Задовільно");
+        break;
+    default:
+        console.log("Незадовільно");
+}
