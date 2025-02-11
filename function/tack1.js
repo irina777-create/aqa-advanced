6ce9eee9da3a264bcbbeb27354777eef5087c95e
irina777-create <grigoreva108@gmail.com>
2025-02-11 19:54:38 +0200

    task1

--- function/tack1.js
+++ function/tack1.js
@@ -1,13 +1,13 @@
 function getRectangleArea(width, height) {
     return width * height;
 }
-console.log(getRectangleArea(5, 10);
+console.log(getRectangleArea(2, 3);
 
 const getRectangleArea = function(width, height) {
     return width * height;
 };
-console.log(getRectangleArea(5, 10));
+console.log(getRectangleArea(3, 5));
 
 const getRectangleArea = (width, height) => width * height;
 
-console.log(getRectangleArea(5, 10));
+console.log(getRectangleArea(4, 5));
