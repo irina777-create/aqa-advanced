a952cbf40c74e66d2bf2e33936748c6a20bc7c32
irina777-create <grigoreva108@gmail.com>
2025-02-22 19:09:43 +0200

    task5

---
+++ Objects/task5.js
@@ -0,0 +1,9 @@
+const users = [
+    { name: "Аліна", email: "alina@example.com", age: 25 },
+    { name: "Петро", email: "petro@example.com", age: 30 },
+    { name: "Anastasya", email: "anastasya@example.com", age: 28 }
+];
+
+for (const { name, email, age } of users) {
+    console.log(`Ім'я: ${name}, Email: ${email}, Вік: ${age}`);
+}
