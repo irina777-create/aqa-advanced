1c7b4a64a4758b4aaf5ba7d92b74629e10484e25
irina777-create <grigoreva108@gmail.com>
2025-02-22 19:03:14 +0200

    task4

---
+++ Objects/task4.js
@@ -0,0 +1,11 @@
+const person = {
+    firstName: "Олександр",
+    lastName: "Іваненко",
+    age: 30
+};
+
+person.email = "oleksandr.ivanenko@example.com";
+
+delete person.age;
+
+console.log(person);
